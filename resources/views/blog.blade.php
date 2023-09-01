499a26ab76952d0aba1f6ffbccd3fd39cbc319a0
Jose Teodoro Ynga Chiroque <jose.ynga@gmail.com>
2023-09-01 01:44:37 -0500

    second commit

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -42,7 +42,7 @@
                                             stroke-width="2" d="m1 1 4 4 4-4" />
                                     </svg>
                                 </button>
-                                <!-- Dropdown menu -->
+
                                 <div id="categoriesNavbar"
                                     class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
                                     <ul class="py-2 text-sm text-gray-700 dark:text-gray-400"
@@ -67,7 +67,7 @@
                                             stroke-width="2" d="m1 1 4 4 4-4" />
                                     </svg>
                                 </button>
-                                <!-- Dropdown menu -->
+
                                 <div id="readabilityNavbar"
                                     class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
                                     <ul class="py-2 text-sm text-gray-700 dark:text-gray-400"
@@ -91,53 +91,10 @@
                     </div>
                 </div>
 
-                {{-- <button data-collapse-toggle="navbar-multi-level" type="button"
-                class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                aria-controls="navbar-multi-level" aria-expanded="false">
-                <span class="sr-only">Open main menu</span>
-                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                    viewBox="0 0 17 14">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M1 1h15M1 7h15M1 13h15" />
-                </svg>
-            </button> --}}
 
-                <div class=" flex justify-between items-center ">
+                <!--                <div class=" flex justify-between items-center ">
                     <div class=" flex justify-between items-center gap-2 mr-4">
-                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20"
-                            height="20" viewBox="0 0 50 50">
-                            <path
-                                d="M41,4H9C6.24,4,4,6.24,4,9v32c0,2.76,2.24,5,5,5h32c2.76,0,5-2.24,5-5V9C46,6.24,43.76,4,41,4z M37,19h-2c-2.14,0-3,0.5-3,2 v3h5l-1,5h-4v15h-5V29h-4v-5h4v-3c0-4,2-7,6-7c2.9,0,4,1,4,1V19z">
-                            </path>
-                        </svg>
-                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20"
-                            height="20" viewBox="0 0 50 50">
-                            <path
-                                d="M 50.0625 10.4375 C 48.214844 11.257813 46.234375 11.808594 44.152344 12.058594 C 46.277344 10.785156 47.910156 8.769531 48.675781 6.371094 C 46.691406 7.546875 44.484375 8.402344 42.144531 8.863281 C 40.269531 6.863281 37.597656 5.617188 34.640625 5.617188 C 28.960938 5.617188 24.355469 10.21875 24.355469 15.898438 C 24.355469 16.703125 24.449219 17.488281 24.625 18.242188 C 16.078125 17.8125 8.503906 13.71875 3.429688 7.496094 C 2.542969 9.019531 2.039063 10.785156 2.039063 12.667969 C 2.039063 16.234375 3.851563 19.382813 6.613281 21.230469 C 4.925781 21.175781 3.339844 20.710938 1.953125 19.941406 C 1.953125 19.984375 1.953125 20.027344 1.953125 20.070313 C 1.953125 25.054688 5.5 29.207031 10.199219 30.15625 C 9.339844 30.390625 8.429688 30.515625 7.492188 30.515625 C 6.828125 30.515625 6.183594 30.453125 5.554688 30.328125 C 6.867188 34.410156 10.664063 37.390625 15.160156 37.472656 C 11.644531 40.230469 7.210938 41.871094 2.390625 41.871094 C 1.558594 41.871094 0.742188 41.824219 -0.0585938 41.726563 C 4.488281 44.648438 9.894531 46.347656 15.703125 46.347656 C 34.617188 46.347656 44.960938 30.679688 44.960938 17.09375 C 44.960938 16.648438 44.949219 16.199219 44.933594 15.761719 C 46.941406 14.3125 48.683594 12.5 50.0625 10.4375 Z">
-                            </path>
-                        </svg>
-                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20"
-                            height="20" viewBox="0 0 30 30">
-                            <path
-                                d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z">
-                            </path>
-                        </svg>
-                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20"
-                            height="20" viewBox="0 0 50 50">
-                            <path
-                                d="M 16 3 C 8.8324839 3 3 8.8324839 3 16 L 3 34 C 3 41.167516 8.8324839 47 16 47 L 34 47 C 41.167516 47 47 41.167516 47 34 L 47 16 C 47 8.8324839 41.167516 3 34 3 L 16 3 z M 16 5 L 34 5 C 40.086484 5 45 9.9135161 45 16 L 45 34 C 45 40.086484 40.086484 45 34 45 L 16 45 C 9.9135161 45 5 40.086484 5 34 L 5 16 C 5 9.9135161 9.9135161 5 16 5 z M 37 11 A 2 2 0 0 0 35 13 A 2 2 0 0 0 37 15 A 2 2 0 0 0 39 13 A 2 2 0 0 0 37 11 z M 25 14 C 18.936712 14 14 18.936712 14 25 C 14 31.063288 18.936712 36 25 36 C 31.063288 36 36 31.063288 36 25 C 36 18.936712 31.063288 14 25 14 z M 25 16 C 29.982407 16 34 20.017593 34 25 C 34 29.982407 29.982407 34 25 34 C 20.017593 34 16 29.982407 16 25 C 16 20.017593 20.017593 16 25 16 z">
-                            </path>
-                        </svg>
-                    </div>
-                    <button
-                        class="bg-[#0065cc] w-[66px] h-[66px] flex justify-center items-center hover:bg-[#0066ccaa]">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                            stroke-width="1.5" stroke="white" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                        </svg>
-                    </button>
-                </div>
+                </div> -->
         </nav>
 
         <div class="bg-[#f5f7f8] flex flex-wrap justify-between py-20 px-44 h-full gap-12">
@@ -146,31 +103,30 @@
                     class="flex flex-col w-[350px] h-[450px] rounded overflow-hidden shadow-lg justify-between bg-white">
                     <div>
                         <?php
-                        echo '<img class="w-full h-[180px]" height="80" width="80" src="data:image/jpeg;base64,' . base64_encode($blog->path) . ' alt="path.jpg""/>';
+                        echo '<img class="w-full h-[180px]" height="80" width="80" src="data:image/jpeg;base64,' . base64_encode($blog->ruta) . ' alt="ruta.jpg""/>';
                         ?>
-                        {{-- <div> --}}
                         <div class="px-6 pt-4">
                             <p class="text-gray-400 text-sm">
-                                {{ $blog->type }}
+                                {{ $blog->tipo }}
                             </p>
                         </div>
                         <div class="px-6 pb-4">
-                            <div class="font-bold text-xl mb-2 h-[60px] ">{{ $blog->name }}</div>
+                            <div class="font-bold text-xl mb-2 h-[60px] ">{{ $blog->nombre }}</div>
                             <div class="text-ellipsis overflow-hidden text-gray-700 text-base h-[100px] ">
-                                {{ $blog->description }}
+                                {{ $blog->descri }}
                             </div>
                         </div>
                     </div>
                     <div class="px-6 pb-4 flex justify-between text-gray-400 text-sm">
                         <div class="flex">
                             <?php
-                            echo '<img  height="20" width="20" src="data:image/jpg;base64,' . base64_encode($blog->pathUser) . ' alt="sus""/>';
+                            echo '<img  height="20" width="20" src="data:image/jpg;base64,' . base64_encode($blog->ruta_u) . ' alt="sus""/>';
                             ?>
-                            <p>{{ $blog->nameUser }}</p>
+                            <p>{{ $blog->nombre_u }}</p>
                         </div>
                         <div>
                             <?php
-                            $originalDate = $blog->date;
+                            $originalDate = $blog->fecha;
                             $day = date('d', strtotime($originalDate));
                             $month = date('m', strtotime($originalDate));
                             $monthName = date('F', mktime(0, 0, 0, $month, 10));
@@ -182,26 +138,7 @@
                     </div>
                 </section>
             @endforeach
-            <section
-                class="flex flex-col w-[350px] h-[400px] rounded overflow-hidden shadow-lg justify-between bg-white">
-                <div class="px-12 pt-12 text-[24px] text-center">
-                    <p>Do the difficult things while they are easy and do the great things while they are small. A
-                        journey of a thousand miles must begin with a single step.</p>
-                </div>
-                <div class="px-8 py-6  flex justify-between text-gray-400">
-                    <div class="flex">
-                        <p>-</p>
-                        <p>LAO TZU</p>
-                    </div>
-                    <div>
-                        <p>October 1, 2017</p>
-                    </div>
-                </div>
-            </section>
         </div>
-        <footer class="h-[200px]">
-
-        </footer>
     </div>
 </body>
 
